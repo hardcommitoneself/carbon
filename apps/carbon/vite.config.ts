a1efecb03741a734045f81a42a040f4db19e675e
hardcommitoneself <hardcommitoneself@gmail.com>
2024-03-31 19:09:32 +1100

    wip: 24-3-31vite migration

---
+++ apps/carbon/vite.config.ts
@@ -0,0 +1,12 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import { installGlobals } from "@remix-run/node";
+
+installGlobals();
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [remix()],
+});
